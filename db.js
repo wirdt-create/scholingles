ff30281ca362b9c033b56a603ab9713c9e0d7a54
Emanuel Velez <emanuelvelezortiz9@gmail.com>
2025-10-09 14:39:10 -0500

    Conexión con base de datos Render configurada

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
-// db.js
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
-  .then(() => console.log("✅ Conectado a PostgreSQL"))
+  .then(() => console.log("✅ Conectado correctamente a PostgreSQL"))
   .catch(err => console.error("❌ Error al conectar a PostgreSQL:", err));
 
 module.exports = pool;
